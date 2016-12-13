f68f5fa3c9d8325f1b4e9a396d9677f69aaddca3
ChargerIIC <ChargerIIC@Yahoo.com>
2016-12-13 15:00:44 -0800

    Gruntfile path fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
         separator: ';'
       },
       dist: {
-        src: ['src/bower/angular/angular.js', 'src/js/vendor/ui-utils.js','src/js/app.js',
+        src: ['../bower_components/angular/angular.js', 'src/js/vendor/ui-utils.js','src/js/app.js',
         'src/js/controllers.js','src/js/directives.js'],
         dest: 'dist/js/<%= pkg.name %>.js'
       }
