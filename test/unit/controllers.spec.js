79b6419efb58ddb543868a3d59a3c5b5750e53c2
ChargerIIC <ChargerIIC@Yahoo.com>
2016-12-14 08:26:54 -0800

    Fix of broken test and addition of new test

--- test/unit/controllers.spec.js
+++ test/unit/controllers.spec.js
@@ -3,13 +3,17 @@ describe('controller specs', function() {
   var $scope;
 
   beforeEach(module('myApp.controllers'));
- 
+
   beforeEach(inject(function($rootScope, $controller) {
     $scope = $rootScope.$new();
     $controller('helloWorldCtrl', {$scope: $scope});
   }));
- 
-  it('should create "name" model with first name "Jane"', function() {
-    expect($scope.name.first).toBe('Jane');
+
+  it('should create "person" model with first name "Jane"', function() {
+    expect($scope.person.firstName).toBe('Jane');
+  });
+
+  it('should create "person" model with last name "Doe"', function() {
+    expect($scope.person.firstName).toBe('Doe');
   });
 });
