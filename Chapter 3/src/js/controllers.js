18e985745f4f56200662e17f3e224114065a0391
ChargerIIC <ChargerIIC@Yahoo.com>
2016-12-15 10:07:24 -0800

    Archiving Chapter 3

--- Chapter 3/src/js/controllers.js
+++ Chapter 3/src/js/controllers.js
@@ -40,29 +40,6 @@ angular.module('myApp.controllers', [])
         }
     };
   })
-.controller('MapCtrl', function ($scope, $timeout) {
-  $scope.myMarkers = [];
-
-    $scope.mapOptions = {
-        center: new google.maps.LatLng(37.782,-122.418),
-        zoom: 4,
-        mapTypeId: google.maps.MapTypeId.SATELLITE
-    };
-
-    var cloudLayer = new google.maps.weather.CloudLayer();
-    $timeout(function(){
-        cloudLayer.setMap($scope.myMap);
-    }, 1000);
-
-    $scope.addMarker = function($event, $params) {
-        $scope.myMarkers.push(new google.maps.Marker({
-            map: $scope.myMap,
-            position: $params[0].latLng
-        }));
-    };
-    $scope.eventBinding = {'map-click':
-       'addMarker($event, $params)'};
-})
 .controller('helloWorldCtrl', function ($scope, $timeout) {
     $scope.person = {
         firstName: "Jane",
