74249d31cb24a5ab7648a3fcfed304e49fe19fcb
ChargerIIC <ChargerIIC@Yahoo.com>
2016-12-15 15:51:50 -0800

    intial animation implmentation

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,4 +1,16 @@
 'use strict';
-angular.module('myApp.controllers', []).controller('NgAnimateCtrl', function ($scope) {
-
-});
+angular.module('myApp.controllers', ['myApp.filters'])
+    .controller('NgAnimateCtrl', ['$scope', function ($scope) {
+        $scope.items = [
+            { text: "call mum", completed: false },
+            { text: "do laundry", completed: false }
+        ];
+        $scope.inputText = "";
+        $scope.addItem = function () {
+            $scope.items.push({
+                text: $scope.inputText,
+                completed: false
+            });
+            $scope.inputText = "";
+        };
+    }]);
--- src/js/filters.js
+++ src/js/filters.js
@@ -1,2 +1,16 @@
 'use strict';
-angular.module("myApp.filters", []);
+angular.module('myApp.filters', [])
+    .filter('active', function() {
+        return function(items) {
+            var filteredItems = [];
+            if(items != null && items.length > 0)
+            {
+              for (var i = 0; i < items.length; i++) {
+                  if (!items[i].completed) {
+                      filteredItems.push(items[i]);
+                  }
+              }
+          }
+            return filteredItems;
+        }
+    });
