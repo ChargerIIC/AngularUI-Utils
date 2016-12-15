72bd0392d0eafb745a93233558375433c5b6baff
ChargerIIC <ChargerIIC@Yahoo.com>
2016-12-15 08:44:47 -0800

    Addition of covering unit tests

--- conf/karma.conf.js
+++ conf/karma.conf.js
@@ -1,14 +1,22 @@
-module.exports = function(config){
+module.exports = function (config) {
     config.set({
-    basePath : '../',
-    files : [
-      'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.22/angular.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.22/angular-mocks.js',
-      'src/js/**/*.js',
-      'test/unit/**/*.js'
-    ],
-    autoWatch : true,
-    frameworks: ['jasmine'],
-    browsers : ['Firefox'],
-    plugins : ['karma-firefox-launcher', 'karma-jasmine']
-})};
+        basePath: '../',
+        files: [
+           'src/bower_components/jquery/jquery.js',
+           'src/bower_components/jquery-ui/ui/jquery-ui.js',
+           'src/bower_components/fullcalendar/fullcalendar.js',
+           'src/bower_components/momentjs/moment.js',
+           'src/bower_components/angular/angular.js',
+           'src/bower_components/angular-ui-calendar/src/calendar.js',
+           'test/lib/angular-mocks.js',
+           'src/js/app.js',
+           'src/js/controllers.js',
+           'src/js/filters.js',
+           'test/unit/**/*.js'
+        ],
+        autoWatch: true,
+        frameworks: ['jasmine'],
+        browsers: ['Chrome'],
+        plugins: ['karma-firefox-launcher', 'karma-jasmine']
+    })
+};
--- test/unit/controllers.spec.js
+++ test/unit/controllers.spec.js
@@ -1,19 +1,24 @@
 'use strict';
-describe('controller specs', function() {
-  var $scope;
+describe('controller specs', function () {
+    var $scope;
+    beforeEach(module('myApp.controllers'));
 
-  beforeEach(module('myApp.controllers'));
+    beforeEach(inject(function ($rootScope, $controller) {
+        $scope = $rootScope.$new();
+        $controller("CalCtrl", { $scope: $scope });
+    }));
 
-  beforeEach(inject(function($rootScope, $controller) {
-    $scope = $rootScope.$new();
-    $controller('helloWorldCtrl', {$scope: $scope});
-  }));
+    it("should contain 'Fun with AngularJS' event", function () {
+        expect($scope.events[1].title).toBe('Fun with AngularJS');
+    });
 
-  it('should create "person" model with first name "Jane"', function() {
-    expect($scope.person.firstName).toBe('Jane');
-  });
+    it("should contain new event after execution of dayCLick function", function () {
+        $scope.dayClick();
+        expect($scope.events[2].title).toBe('new event');
+    });
 
-  it('should create "person" model with last name "Doe"', function() {
-    expect($scope.person.lastName).toBe('Doe');
-  });
+    it("should remove an event", function () {
+        $scope.remove(2);
+        expect($scope.events.length).toBe(2);
+    });
 });
