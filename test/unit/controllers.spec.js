dc9c8cb045b888f08b36e8d9141af4d9de4c3e55
ChargerIIC <ChargerIIC@Yahoo.com>
2016-12-15 10:00:02 -0800

    Removing this $scope-$scope nonesense

--- test/unit/controllers.spec.js
+++ test/unit/controllers.spec.js
@@ -1,25 +1,25 @@
 'use strict';
-var $scope;
+var scope;
 
 describe('controller specs', function () {
     beforeEach(module('myApp.controllers'));
 
     beforeEach(inject(function ($rootScope, $controller) {
-        $scope = $rootScope.$new();
-        $controller("CalCtrl", { $scope: $scope });
+        scope = $rootScope.$new();
+        $controller("CalCtrl", { $scope: scope });
     }));
 
     it("should contain 'Fun with AngularJS' event", function () {
-        expect($scope.events[1].title).toBe('Fun with AngularJS');
+        expect(scope.events[1].title).toBe('Fun with AngularJS');
     });
 
     it("should contain new event after execution of dayCLick function", function () {
-        $scope.dayClick();
-        expect($scope.events[2].title).toBe('new event');
+        scope.dayClick();
+        expect(scope.events[2].title).toBe('new event');
     });
 
     it("should remove an event", function () {
-        $scope.remove(2);
-        expect($scope.events.length).toBe(2);
+        scope.remove(2);
+        expect(scope.events.length).toBe(2);
     });
 });
