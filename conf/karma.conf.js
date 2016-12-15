de9729d6b0346adebc12d80f7c860e2c8fd6553b
ChargerIIC <ChargerIIC@Yahoo.com>
2016-12-15 12:08:29 -0800

    Accidently left dashes in the path, which breaks CI

--- conf/karma.conf.js
+++ conf/karma.conf.js
@@ -2,9 +2,9 @@ module.exports = function (config) {
     config.set({
         basePath: '../',
         files: [
-            '/bower_components/jquery/jquery.js',
-            '/bower_components/angular/angular.js',
-            '/bower_components/angular-mocks/angular-mocks.js',
+            'bower_components/jquery/jquery.js',
+            'bower_components/angular/angular.js',
+            'bower_components/angular-mocks/angular-mocks.js',
             'src/js/app.js',
             'src/js/controllers.js',
             'src/js/filters.js',
