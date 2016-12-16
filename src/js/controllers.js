c61083886e0aa9f1c48cedcd5dd5c2e8a55550d6
ChargerIIC <ChargerIIC@Yahoo.com>
2016-12-16 11:36:55 -0800

    Additional animations and inclusion of modernizer

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -16,4 +16,14 @@ angular.module('myApp.controllers', ['myApp.filters'])
            }
           $scope.inputText = "";
         };
-}]);
+}])
+.animation('.item', function() {
+    return {
+        enter: function(element, done) {
+            element.css({'opacity': 0, 'margin-left': '-230px'});
+            element.animate({'opacity': 1, 'margin-left': 0}, 500, done);
+        },
+        leave: function(element, className, done) {element.animate({'opacity': 0, 'margin-left': -230}, 500, done);
+        }
+    }
+ });
