9da85c18d0a5a2a83f8d54fc4ef3e94a80de8013
ChargerIIC <ChargerIIC@Yahoo.com>
2016-12-20 07:52:02 -0800

    Implementation of Chapter 9

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,34 +1,26 @@
-angular.module('myApp.controllers', []).controller('ViewIssueCtrl', function($scope, $route, $element, $timeout) {
-  $scope.$watch(function() { return $route.current && $route.current.params.issue; }, function(newVal, oldVal) {
-    $scope.showTab = !!newVal;
-    $scope.issueId = newVal;
+angular.module('myApp.controllers', []).controller('GooglePaginationCtrl', function($scope, $templateCache) {
 
-    $timeout(function() {
-      if ($scope.showTab) {
-        $element.find('a').click();
-      } else {
-        $element.siblings().first().find('a').click();
-      }
-    }, 0, false);
-  });
+  $templateCache.put("template/pagination/pagination.html",
+    '<table class="google-pagination">\
+    <tbody>\
+      <tr valign="top">\
+        <td><td>\
+          <span ng-if="noPrevious()" class="csb noprev"></span>\
+          <a -if="!noPrevious()" ng-click="selectPage(page - 1)"><span class="csb prev"></span></a>\
+        </td>\</td>\
+        <td><td ng-repeat="page in pages track by $index" ng-click="selectPage(page.number)">\
+          <span ng-if="page.active"><span class="csb active"></span>{{page.text}}</span>\
+          <a ng-if="!page.active"><span class="csb nonactive"></span>{{page.text}}</a>\
+        </td>\</td>\
+        <td><td>\
+          <span ng-if="noNext()" class="csb nonext"></span>\
+          <a href ng-if="!noNext()" ng-click="selectPage(page + 1)"><span class="csb next"></span></a>\
+        </td>\</td>\
+      </tr>\
+    </tbody>\
+  </table>'
+  );
 
-  $scope.carousel = [
-    { image: 'images/1.png' },
-    { image: 'images/2.png' },
-    { image: 'images/3.png' }
-  ];
-}).controller('FindCtrl', function($scope, $location) {
-  $scope.issues = [
-    {'id': 16877, 'name': '#16877'},
-    {'id': 1030, 'name': '#1030'},
-    {'id': 2039, 'name': '#2039'},
-    {'id': 1134, 'name': '#1134'},
-    {'id': 1078, 'name': '#1078'},
-    {'id': 2690, 'name': '#2690'}
-  ];
-
-  $scope.onSelect = function(item) {
-    $location.path('/issue/' + item.id);
-    $scope.find = '';
-  };
+  $scope.totalItems = 100;
+  $scope.currentPage = 2;
 });
