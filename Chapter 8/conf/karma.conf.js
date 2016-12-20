3739767cf99111000e99eed6a0e908450e37ac3c
ChargerIIC <ChargerIIC@Yahoo.com>
2016-12-20 07:24:15 -0800

    Archiving Chapter 8 code

---
+++ Chapter 8/conf/karma.conf.js
@@ -0,0 +1,18 @@
+module.exports = function (config) {
+    config.set({
+        basePath: '../',
+        files: [
+            'bower_components/jquery/jquery.js',
+            'bower_components/angular/angular.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            'src/js/app.js',
+            'src/js/controllers.js',
+            'src/js/filters.js',
+            'test/unit/**/*.js'
+        ],
+        autoWatch: true,
+        frameworks: ['jasmine'],
+        browsers: ['Firefox'],
+        plugins: ['karma-firefox-launcher', 'karma-jasmine']
+    })
+};
